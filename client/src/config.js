ee9358bfff261452b679a23d6a51bd46325038d0
Aditya <adityasapurba@gmail.com>
2023-11-30 17:36:40 +0530

    adding config

---
+++ client/src/config.js
@@ -0,0 +1,11 @@
+export const API = "https://casa-byte.vercel.app/api";
+export const FIREBASE_API_KEY = "AIzaSyDDK5O3i0wU-Ra54BEpxIJWlzhX_XV5kgU";
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyDDK5O3i0wU-Ra54BEpxIJWlzhX_XV5kgU",
+    authDomain: "realist-db192.firebaseapp.com",
+    projectId: "realist-db192",
+    storageBucket: "realist-db192.appspot.com",
+    messagingSenderId: "941883088980",
+    appId: "1:941883088980:web:1597f631e0f62b2b8d4dc1",
+    measurementId: "G-5GW7D1WKJZ"
+  };
